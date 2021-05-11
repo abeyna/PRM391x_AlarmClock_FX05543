383e6ea8d66fb227f6cb0617064e6b8ecf63378d
Luan Nguyen <67037684+abeyna@users.noreply.github.com>
2021-05-11 09:42:31 +0700

    option2 commit 5.0

--- app/src/main/java/funix/prm/prm391x_alarmclock_fx05543/AlarmReceiver.java
+++ app/src/main/java/funix/prm/prm391x_alarmclock_fx05543/AlarmReceiver.java
@@ -8,6 +8,9 @@ import android.widget.Toast;
 public class AlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+        Intent myIntent = new Intent(context, RingtoneService.class);
+        context.startService(myIntent);
+
         Toast.makeText(context.getApplicationContext(), "Alarm is ringing", Toast.LENGTH_SHORT).show();
     }
 }
